d4785d9836f64485f043cd6310ffaf77d255af24
Anas Aboureada <anas.ieee@gmail.com>
2018-03-02 03:33:51 +0200

    Edited play function to make it possible to control the number of players and diceroll

--- game.js
+++ game.js
@@ -169,18 +169,19 @@ exports.Game = function() {
 		return true;
   };
 
-  this.play = function (){
+  this.play = function (playersCount, diceRoll){
     var notAWinner = false;
 
     var game = new Game();
 
-    game.add('Chet');
-    game.add('Pat');
-    game.add('Sue');
+    // Add new players
+    for (var i = 1; i < playersCount + 1; i += 1) {
+      var playerName = "player_" + i;
+      game.add(playerName);
+    }
 
     do{
-
-      game.roll(Math.floor(Math.random()*6) + 1);
+      game.roll(diceRoll || Math.floor(Math.random()*6) + 1);
 
       if(Math.floor(Math.random()*10) == 7){
         notAWinner = game.wrongAnswer();
